c8ab6f4f2ee9d153c67278daa377c475d7cf2ab6
MarshallJoseph <mj15hh@brocku.ca>
2019-11-28 13:45:35 -0500

    Added activity_visualizer and fragment_visualizer xml files

--- app/src/main/java/ca/brocku/songly/MainActivity.java
+++ app/src/main/java/ca/brocku/songly/MainActivity.java
@@ -3,13 +3,27 @@ package ca.brocku.songly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set content to activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Hides the action bar
+        getSupportActionBar().hide();
+
+        // Hides the status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+
     }
 
     private void test() {
