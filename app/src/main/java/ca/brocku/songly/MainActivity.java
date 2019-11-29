a3e8ba737bc68e5c08bf54b57edf90bbcba9e0ef
MarshallJoseph <mrmarshalljoseph@gmail.com>
2019-11-29 11:09:51 -0500

    message

--- app/src/main/java/ca/brocku/songly/MainActivity.java
+++ app/src/main/java/ca/brocku/songly/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button visualizer, songly, studio;
-
+    // make change
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
