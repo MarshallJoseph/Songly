efa1506d3a6e35e44c79ce9329cf2c73c88fc302
HawtPie <42501903+HawtPie@users.noreply.github.com>
2019-12-04 13:53:32 -0500

    Made changeable layouts for Songly. Added instrumental/lyric files for all songs

--- app/src/main/java/ca/brocku/songly/SonglyActivity.java
+++ app/src/main/java/ca/brocku/songly/SonglyActivity.java
@@ -2,6 +2,7 @@ package ca.brocku.songly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.media.TimedText;
 import android.os.Bundle;
@@ -23,15 +24,32 @@ public class SonglyActivity extends AppCompatActivity implements MediaPlayer.OnT
     private static Handler handler = new Handler();
     TextView lyrics;
     ImageView sButt,plButt,paButt,rButt;
-    MediaPlayer player;
+    static MediaPlayer player;
 
     int currTrackIndex;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songly);
+
         lyrics = (TextView) findViewById(R.id.lyrics);
-        player = MediaPlayer.create(this, R.raw.never);
+        //For carrying over lyrics when switching to landscape
+        String carryOver = "";
+        if (savedInstanceState!=null)
+            carryOver = savedInstanceState.getString("currentDisplay");
+        lyrics.setText(carryOver);
+
+        // Hides the action bar
+        getSupportActionBar().hide();
+
+        // Hides the status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        if(player==null) {
+            player = MediaPlayer.create(this, R.raw.neverinst);
+        }
         try {
             player.addTimedTextSource(getSubtitleFile(R.raw.neverlyrics), MediaPlayer.MEDIA_MIMETYPE_TEXT_SUBRIP);
             int textTrackIndex = findTrackIndexFor(
@@ -44,11 +62,17 @@ public class SonglyActivity extends AppCompatActivity implements MediaPlayer.OnT
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         sButt = findViewById(R.id.stop);
         plButt = findViewById(R.id.play);
         rButt = findViewById(R.id.restart);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle stuff) {
+        stuff.putString("currentDisplay",(String)lyrics.getText());
+        super.onSaveInstanceState(stuff);
+    }
+
     //Finds index needed to pair TimedText with
     private int findTrackIndexFor(int mediaTrackType, MediaPlayer.TrackInfo[] trackInfo) {
         int index = -1;
@@ -130,12 +154,6 @@ public class SonglyActivity extends AppCompatActivity implements MediaPlayer.OnT
             player.release();
             player = null;
         }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        stopPlayer();
     }
 
     public void restart(View v){
